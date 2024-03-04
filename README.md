fc522919c0ff3d5d47e9e754b372fce0d202b94a
santiblaumann <sb2239@cornell.edu>
2024-03-04 15:33:10 -0500

    Implemented Very Basic Bot Functionality
    - Connects with Guilded Server, sending a message when initialized
    - Can distinguish between a snipe and a regular message

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ In Cornell Culture (and college campuses nationally, I'd assume), a snipe is: wh
 To up the ante, this bot is designed to keep a count of the "kill/death" ratio for snipes. When someone is tagged with an accompanying snipe picture, they gain one "kill" while the receipient receives a "death".
 
 ## Installation
-This bot is meant for use in Guilded servers, as it is what we use on CU Autonomous Drone. I've read that the Guilded bots are designed to be very easy to turn into Discord bots and vice versa, but **this is not a discord bot.**
+This bot is meant for use in Guilded servers, as it is what we use on CU Autonomous Drone. Guilded bots are designed to be very easy to turn into Discord bots and vice versa, but **this is not a discord bot.**
 
 You need to be running Python 3.8 or above, and must have the guilded.py package installed on your machine. To install it, run the following command:
 
--- bot.py
+++ bot.py
@@ -1,4 +1,29 @@
-import requests, guilded.py  # i hate python import bullshit
+import requests, guilded, asyncio # i hate python import bullshit
+from guilded.ext import commands
+
+token = ''  # apikey.txt
+snipeBot = guilded.Client()
+
+allowed_channels = []  # also in apikey.txt
+# when a message is sent into snipes, check if the bot should act on it. 
+
+@snipeBot.event
+async def on_ready():
+    print("Bot is ready.")
+    mes = await snipeBot.fetch_channel(allowed_channels[1])
+    await mes.send(f"Bot is initialized and is listening...")
 
 
+@snipeBot.event
+async def on_message(message):
+    # check the following: robot isn't replying to itself, this message is in snipes, has an attachment, and an @ to tag someone
+    if (message.author.id == snipeBot.user.id or message.channel_id not in allowed_channels  
+        or len(message.attachments) == 0 or len(message.mentions) == 0):
+        print("no snipe")
+        # await message.channel.send(f"Message would not trigger a snipe...")
+        return
+    else:
+        print("A snipe has occured...")
+        await message.channel.send(f"Snipe!")
 
+snipeBot.run(token)
